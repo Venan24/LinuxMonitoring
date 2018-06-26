f9bf984ef821de89f9cdae663f22a7739147df68
Venan24 <venanosmic24@gmail.com>
2018-06-26 02:08:27 +0200

    Get last 12 records in graph (1 min)

--- rest/v1/index.php
+++ rest/v1/index.php
@@ -179,7 +179,7 @@ Flight::route('POST /checkauth', function () {
 Flight::route('GET /getgraph/@auth/@token', function ($auth, $token) {
   try {
       $token = (array)JWT::decode($token, Config::JWT_SECRET, ['HS256'])->user;
-      $data = Flight::pm()->query("SELECT * FROM Monitoring WHERE auth_code = :auth ORDER BY id ASC LIMIT 36", [':auth' => $auth]);
+      $data = Flight::pm()->query("SELECT * FROM Monitoring WHERE auth_code = :auth ORDER BY id ASC LIMIT 12", [':auth' => $auth]);
       Flight::json($data);
   } catch (Exception $e) {
     Flight::json($e);
