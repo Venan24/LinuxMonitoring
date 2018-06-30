3e4eb38b2255d16fafd15dcfcc15019249cd9d32
Venan24 <venanosmic24@gmail.com>
2018-06-30 02:24:14 +0200

    Auth token send as parameter

--- rest/v1/index.php
+++ rest/v1/index.php
@@ -144,7 +144,7 @@ Flight::route('POST /endpoint', function () {
     if ($row_cnt == 1) {
         $unos = Flight::pm()->insert_monitor_data($auth_code, $os_name, $os_version, $cpu_model, $cpu_architecture, $cpu_cores, $cpu_threads, $cpu_percentage, $hostname, $internal_ip, $external_ip, $ram_total, $ram_used, $ram_free, $ram_shared, $ram_available, $ram_buff, $swap_total, $swap_used, $swap_free, $total_hdd, $used_hdd, $available_hdd, $pid_running, $uptime, $timesubmited);
         if ($unos) {
-            print_r("Successfully sent to DB");
+            print_r("Successfully sent to DB at: ".$timesubmited);
         } else {
             print_r("Error");
         }
@@ -256,9 +256,9 @@ Flight::route('GET /user/@email', function ($email) {
 });
 
 //Check if authorization token is valid
-Flight::route('GET /provjeritoken/@token', function ($token) {
+Flight::route('GET /provjeritoken', function () {
     try {
-        //$token = Flight::request()->data->token;
+        $token = Flight::request()->query->authtoken;
         $user = (array)JWT::decode($token, Config::JWT_SECRET, ['HS256']);
         Flight::json(array('Authorized' => true));
     } catch (Exception $e) {
